692c5dcae321a4677ed7ed3fce914bf919b82a10
venettacoreid2-crypto <venettacore.id2@gmail.com>
2026-01-11 16:41:22 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -14,9 +14,9 @@ export default {
       );
     }
 
-    if (url.pathname === "login") return login(req, env);
-    if (url.pathname === "verify-otp") return verifyOtp(req, env);
-    if (url.pathname === "session") return session(req, env);
+    if (url.pathname === "/login") return login(req, env);
+    if (url.pathname === "/verify-otp") return verifyOtp(req, env);
+    if (url.pathname === "/session") return session(req, env);
 
     return new Response("Not Found", { status: 404 });
   }
